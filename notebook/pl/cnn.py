dfa4fe71690b7906a02e864ad14b2649ebf280f9
aemwx-user1 <aemwx-user1@login1.pi.sjtu.edu.cn>
2022-05-25 14:12:53 +0800

    .

---
+++ notebook/pl/cnn.py
@@ -0,0 +1,99 @@
+import sys
+sys.path.append('/lustre/home/acct-aemwx/aemwx-user1/wangyu/my/nlp-final')
+import numpy as np
+import pandas as pd
+import config
+import torch
+import matplotlib.pyplot as plt
+import random
+from torch.nn import functional as F
+import wandb
+from pytorch_lightning.loggers import WandbLogger
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader
+from pl_bolts.datamodules import SklearnDataset
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.callbacks import EarlyStopping
+import torchmetrics as tm
+import warnings
+warnings.filterwarnings('ignore')
+from models.model import *
+gpu = False
+random.seed(999)
+
+wandb.login()
+wandb_logger = WandbLogger(project='cnn')
+
+class MyDataModule(pl.LightningDataModule):
+
+    def __init__(self, data_dir: str = config.DATA_DIR, batch_size: int = 64, num_workers: int = 4,fraction_rate: float = 0.8,val_fraction_rate: float = 0.1):
+        super().__init__()
+        self.data_dir = data_dir
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.num_classes = 10
+        self.fraction_rate = fraction_rate
+        self.val_fraction_rate = val_fraction_rate
+        
+        data0 = torch.load(self.data_dir+'/embedding_dict_data0.pth')
+        data0 =torch.cat([data0,torch.zeros(len(data0),1)],1)
+        data1 = torch.load(self.data_dir+'/embedding_dict_data1.pth')
+        data1 = torch.cat([data1,torch.ones(len(data1),1)],1)
+        data = torch.cat([data0,data1],0)
+        index = [i for i in range(len(data))]
+        random.shuffle(index)
+        data = data[index,:]
+        self.data = data.detach().numpy()
+    def prepare_data(self):
+        pass
+    def setup(self, stage=None):
+        train_test_split = int(self.fraction_rate*len(self.data))
+        insample = self.data[:train_test_split,:]
+        test_data = self.data[train_test_split:,:]
+        train_val_split = int((1-self.val_fraction_rate)*len(insample))
+        train_data = insample[:train_val_split,:]
+        val_data  = insample[train_val_split:,:]
+        self.dataset_train = SklearnDataset(X=train_data[:,:-1],y = train_data[:,-1].astype(int))
+        self.dataset_val = SklearnDataset(X=val_data[:,:-1],y = val_data[:,-1].astype(int))
+        self.dataset_test = SklearnDataset(X=test_data[:,:-1],y = test_data[:,-1].astype(int))
+    def train_dataloader(self):
+        return DataLoader(self.dataset_train, batch_size=self.batch_size, num_workers=self.num_workers,pin_memory=gpu)
+
+    def val_dataloader(self):
+        return DataLoader(self.dataset_val, batch_size=self.batch_size, num_workers=self.num_workers,pin_memory=gpu)
+
+    def test_dataloader(self):
+        return DataLoader(self.dataset_test, batch_size=self.batch_size, num_workers=self.num_workers,pin_memory=gpu)
+
+if __name__=='__main__':
+    checkpoint_callback = ModelCheckpoint(
+        monitor='val_loss',
+        filename='sample-mnist-{epoch:02d}-{val_loss:.2f}',
+        save_top_k=3,
+        mode='min',
+        save_last=True
+    )
+    early_stop_callback = EarlyStopping(monitor="val_loss",
+     min_delta=0.00,
+     patience=10,
+     verbose=False,
+     mode="min")
+    
+    trainer = pl.Trainer(accelerator='auto',
+                         max_epochs=100,
+                        callbacks=[checkpoint_callback,
+                                   early_stop_callback,
+                                   ],
+                        logger=wandb_logger,
+                        strategy="fsdp",
+                        check_val_every_n_epoch=1,
+                        devices=6,
+                        precision=16,
+                        )
+    model = CNNModel(lr=0.01)
+    data_module = MyDataModule(batch_size=256,num_workers=10)
+    trainer.fit(model,data_module)
+    trainer.test(model, datamodule=data_module)
+    wandb.finish()
+
+
--- notebook/pl/models/model.py
+++ notebook/pl/models/model.py
@@ -2,7 +2,7 @@ import pytorch_lightning as pl
 import torch
 import torch.nn.functional as F
 import torchmetrics as tm
-
+from torch import nn
 
 class BaseLineModel(pl.LightningModule):
 
@@ -19,6 +19,76 @@ class BaseLineModel(pl.LightningModule):
         x = x.view(x.size(0), -1)
         x = self.l1(x)
         x = self.sm(x)
+        return x
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        x = self(x).squeeze(1)
+        loss = self.calc_loss(x, y.float())
+        self.log('train_loss',loss)
+        self.log('train_acc',self.accuracy(x,y))
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        x = self(x).squeeze(1)
+        loss = self.calc_loss(x, y.float())
+        self.log('val_loss', loss)
+        self.log('val_acc',self.accuracy(x,y))
+        
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        x = self(x).squeeze(1)
+        loss = self.calc_loss(x, y.float())
+        self.log('test_loss', loss)
+        self.log('test_acc',self.accuracy(x,y))
+        
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=self.lr)
+    
+    
+    
+    
+    
+class CNNModel(pl.LightningModule):
+
+    def __init__(self,n_feat =300,num_class=1,lr=0.01):
+        super().__init__()
+        self.accuracy = tm.Accuracy()
+        self.save_hyperparameters()
+        self.lr = lr
+        
+        self.calc_loss = torch.nn.BCEWithLogitsLoss()
+        
+        
+        self.f1 = nn.Linear(300,)
+        
+        
+        self.conv1 = nn.Conv1d(in_channels=n_feat, out_channels=8, kernel_size=3)
+        self.maxpool1 = nn.MaxPool1d(n_feat-3+1)
+        self.conv2 = nn.Conv1d(in_channels=8, out_channels=8, kernel_size=3)
+        self.maxpool2 = nn.MaxPool1d(n_feat-3+1)
+        self.conv3 = nn.Conv1d(in_channels=8, out_channels=8, kernel_size=3)
+        self.maxpool3 = nn.MaxPool1d(n_feat-3+1)
+        self.concat = torch.cat()
+        self.out = nn.Linear(1000,1)
+        self.sig = nn.Sigmoid()
+        
+    def forward(self, x):
+        x = x.view(x.size(0), -1)
+        x1 = self.conv1(x)
+        x11 = self.maxpool1(x1)
+        x2 = self.conv2(x)
+        x21 = self.maxpool2(x2)
+        x3 = self.conv3(x)
+        x31 = self.maxpool3(x3)
+        b, c, d, h, w = x11.size()
+        x12 = x11.view(-1, c * d * h * w)
+        x22 = x21.view(-1, c * d * h * w)
+        x32 = x31.view(-1, c * d * h * w)
+        x4 = self.concat(x12,x22,x32)
+        x5 = self.out(x4)
+        x = self.sig(x5)
+        
         return x
     def training_step(self, batch, batch_idx):
         x, y = batch
